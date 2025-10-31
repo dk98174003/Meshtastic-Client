80bd4ea11805759549cacbc77d1cbbd529787a94
Knud Schrøder <37827873+dk98174003@users.noreply.github.com>
2025-10-31 15:02:49 +0100

    Add files via upload

---
+++ compile.bat
@@ -0,0 +1,5 @@
+pyinstaller --clean --noconsole --onefile --name "MeshtasticClient" ^
+  --icon="D:\SynologyDrive\GIT\Meshtastic\Meshtastic_client\meshtastic.ico" ^
+  --add-data "D:\SynologyDrive\GIT\Meshtastic\Meshtastic_client\meshtastic.ico;." ^
+  "D:\SynologyDrive\GIT\Meshtastic\Meshtastic_client\meshtastic_client.py"
+
